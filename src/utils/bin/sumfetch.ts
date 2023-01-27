6464944faf163ae6b35e3f1d926c32aab67044a1
user name <hello@singsongaftermath.com>
2023-01-26 17:58:50 -0800

    Initial Commit

---
+++ src/utils/bin/sumfetch.ts
@@ -0,0 +1,46 @@
+import config from '../../../config.json';
+
+const sumfetch = async (args: string[]): Promise<string> => {
+  if (config.ascii === 'cveinnt') {
+    return `                                                  
+             @@@@@@@@@@@@@                   sumfetch: summary display
+        @@@@               @@@@             -----------
+      @@                       @@            ABOUT
+    @@                           @@          ${config.name}
+  @@                               @@       ﰩ ${config.ps1_hostname}
+ @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+@@                                   @@     -----------
+@@             .@@@@@@@@@@.          @@      CONTACT 
+ @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+     @@@                        @@@         -----------
+        @@@                  @@@ @@         ❝❞ QUOTE: 
+         @|  @@@@@@@@@@@@@@@@   @@          Always grinding... 
+         @|                      @@         
+
+`;
+  } else {
+    return `
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+▐▓                                 ▐▓       -----------
+▐▓        > L I V E T E R M        ▐▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            ❝❞ QUOTE:
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               Always grinding... 
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
+
+`;
+  }
+};
+
+export default sumfetch;
